8ae7faffe32061d423045d2238f4116ece631cb0
priyeh-zero <priyeh.zero@gmail.com>
2020-05-28 22:29:28 +0530

    removed host

--- modules/helper.functions.js
+++ modules/helper.functions.js
@@ -1,49 +1,45 @@
-var nodemailer = require('nodemailer');
+var nodemailer = require("nodemailer");
 
-const { EMAIL, PASSWORD, DISPLAY_NAME } = process.env
-
+const { EMAIL, PASSWORD, DISPLAY_NAME } = process.env;
 
 // Create a transporter for the mailer
 
 var transporter = nodemailer.createTransport({
-  host: "mail.mechlintech.com", // hostname
+  host: "", // hostname
   secureConnection: true, // TLS requires secureConnection to be false
   port: 465, // port for secure SMTP
   tls: {
-	  //ciphers:'SSLv3',
-     rejectUnauthorized: false
+    //ciphers:'SSLv3',
+    rejectUnauthorized: false,
   },
   auth: {
     user: EMAIL,
-    pass: PASSWORD
-  }
+    pass: PASSWORD,
+  },
 });
 
-
 const sendMail = async (sendEmail, emailMessage) => {
-
   // setup e-mail data, even with unicode symbols
   var mailOptions = {
-    from: '"'+DISPLAY_NAME+'" <'+EMAIL+'>', // sender address (who sends)
+    from: '"' + DISPLAY_NAME + '" <' + EMAIL + ">", // sender address (who sends)
     to: sendEmail, // list of receivers (who receives)
-    subject: 'Broken Link', // Subject line
+    subject: "Broken Link", // Subject line
     // text: 'Hello world ', // plaintext body
-    html: emailMessage // html body
+    html: emailMessage, // html body
   };
 
   try {
-    await transporter.sendMail(mailOptions)
+    await transporter.sendMail(mailOptions);
   } catch (exp) {
-
-    console.log(`Sending Email to ${sendEmail} failed! \n ${exp}`)
+    console.log(`Sending Email to ${sendEmail} failed! \n ${exp}`);
   }
-}
+};
 
 // reports
 const defaultReportTemplate = (url, statusCode, broken) => ({
-    url,
-    statusCode,
-    broken
-})
+  url,
+  statusCode,
+  broken,
+});
 
-module.exports = { defaultReportTemplate, sendMail }
+module.exports = { defaultReportTemplate, sendMail };
